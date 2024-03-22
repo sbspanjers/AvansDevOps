fc7838153da449b61ec50d77c34ac11995ab907c
StanTop <saj.tophoven@student.avans.nl>
2024-03-22 11:36:40 +0100

    more tests of the sprint and adapters and changed pipeline method to virtual

--- AvansDevOps/Domain/Models/Pipeline.cs
+++ AvansDevOps/Domain/Models/Pipeline.cs
@@ -15,7 +15,7 @@ public class Pipeline
         this.devOps = new DevOpsAdapter();
     }
 
-    public bool Start(Sprint sprint)
+    public virtual bool Start(Sprint sprint)
     {
         int random = new Random().Next(0, 2); 
         // can go well
